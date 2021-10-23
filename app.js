705de9555a0eeaebb327da92bc7c66ed67bf7de9
Séraphin <fatet.seraphin@gmail.com>
2021-10-23 14:50:29 +0200

    Update Player

--- app.js
+++ app.js
@@ -24,22 +24,40 @@ server.listen(3000, () => {
 // =====================================================================
 // Global Variables & Classes
 // =====================================================================
+const DEBUG = true;
+
 const playerMax = 100;
 let socketList = {};
 let playerList = {};
 
+const collision = (first, second) => {
+   if(!(first.x > second.x + second.width
+      ||first.x + first.width < second.x
+      ||first.y > second.y + second.height
+      ||first.y + first.height < second.y)) {
+         return true;
+   }
+}
+
 class Player {
    constructor(id) {
       this.id = id;
+      this.speed = 2;
+      this.runSpeed = 15;
+      this.diagSpeed = this.speed * 0.7;
+      this.health = 100;
+
       this.x = 150;
       this.y = 100;
       this.width = 80;
       this.height = 80;
-      this.speed = 10;
+
       this.up = false;
       this.down = false;
       this.left = false;
       this.right = false;
+
+      this.isRunning = false;
    }
 
    update() {
@@ -47,7 +65,56 @@ class Player {
       if(this.down) this.y += this.speed;
       if(this.left) this.x -= this.speed;
       if(this.right) this.x += this.speed;
+
+      if(this.up && this.left
+      ||this.up && this.right
+      ||this.down && this.left
+      ||this.down && this.right) {
+         this.speed = this.diagSpeed;
+      }
+
+      if(this.isRunning) this.speed = this.runSpeed;
+      // if(!this.isRunning) this.speed = this.runSpeed;
    }
+}
+
+
+// =====================================================================
+// Static functions
+// =====================================================================
+Player.onConnect = (socket) => {
+   const player = new Player(socket.id);
+   playerList[socket.id] = player;
+
+   socket.on("up", (data) => player.up = data);
+   socket.on("down", (data) => player.down = data);
+   socket.on("left", (data) => player.left = data);
+   socket.on("right", (data) => player.right = data);
+   socket.on("running", (data) => player.isRunning = data);
+}
+
+Player.OnDisconnect = (socket) => {
+   delete playerList[socket.id];
+}
+
+Player.updatePosition = () => {
+   let pack = [];
+
+   for(let i in playerList) {
+      let player = playerList[i];
+
+      for(let j in playerList) {
+         let enemy = playerList[j];
+
+         if(enemy !== player && collision(player, enemy)) {
+            player.health -= 0.1;
+         }
+      }
+      
+      player.update();
+      pack.push(player);
+   }
+   return pack;
 }
 
 
@@ -57,25 +124,34 @@ class Player {
 io.on("connection", (socket) => {
    console.log("User connected !");  
 
+   // ==========  Generate ID  ==========
    socket.id = Math.floor(playerMax * Math.random());
    socketList[socket.id] = socket;
-
-   const player = new Player(socket.id);
-   playerList[socket.id] = player;
+   Player.onConnect(socket);   
    
+
+   // ==========  Disconnection  ==========
    socket.on("disconnect", () => {
       console.log("User disconnected !");
       delete socketList[socket.id];
-      delete playerList[socket.id];
+      Player.OnDisconnect(socket);
    });
-   
-   socket.x = 0;
-   socket.y = 0;
 
-   socket.on("up", (data) => player.up = data);
-   socket.on("down", (data) => player.down = data);
-   socket.on("left", (data) => player.left = data);
-   socket.on("right", (data) => player.right = data);
+
+   // ==========  Chatting  ==========
+   socket.on("sendMessage", (data) => {
+      const playerName = socket.id;
+      for(let i in socketList) {
+         socketList[i].emit("addMessage", `${playerName} : ${data}`)
+      }
+   });
+
+   // ==========  Debugging  ==========
+   socket.on("evalServer", (data) => {
+      if(!DEBUG) return;
+      const response = eval(data);
+      socket.emit("evalResponse", response);
+   });
 });
 
 
@@ -83,19 +159,10 @@ io.on("connection", (socket) => {
 // Sync
 // =====================================================================
 setInterval(() => {
-   let pack = [];
-
-   for(let i in playerList) {
-      let player = playerList[i];
-      player.update();
-      pack.push({
-         x: player.x,
-         y: player.y,
-      });
-   }
-
+   let pack = Player.updatePosition();
+   
    for(let i in socketList) {
       let socket = socketList[i];
       socket.emit("newPosition", pack);
    }
-}, 1000/30);
+}, 1000/60);
--- client/JavaScript/gameHandler.js
+++ client/JavaScript/gameHandler.js
@@ -1,36 +1,45 @@
 
+// =====================================================================
+// Global variables
+// =====================================================================
 const canvas = document.querySelector(".canvas-1");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 500;
+canvas.width = 1000;
+canvas.height = 700;
 
-
-// =====================================================================
-// Variables
-// =====================================================================
 const socket = io();
 
+// Controls
 const up = "z";
 const down = "s";
 const left = "q";
 const right = "d";
+const run = " ";
+
+let isChatting = false;
 
 
 // =====================================================================
 // Movements
 // =====================================================================
 window.addEventListener("keydown", (event) => {
-   if(event.key === up) socket.emit("up", true);
-   if(event.key === down) socket.emit("down", true);
-   if(event.key === left) socket.emit("left", true);
-   if(event.key === right) socket.emit("right", true);
+   if(!isChatting) {
+      if(event.key === up) socket.emit("up", true);
+      if(event.key === down) socket.emit("down", true);
+      if(event.key === left) socket.emit("left", true);
+      if(event.key === right) socket.emit("right", true);
+      if(event.key === run) socket.emit("running", true);
+   }
 });
-
+   
 window.addEventListener("keyup", (event) => {
-   if(event.key === up) socket.emit("up", false);
-   if(event.key === down) socket.emit("down", false);
-   if(event.key === left) socket.emit("left", false);
-   if(event.key === right) socket.emit("right", false);
+   if(!isChatting) {
+      if(event.key === up) socket.emit("up", false);
+      if(event.key === down) socket.emit("down", false);
+      if(event.key === left) socket.emit("left", false);
+      if(event.key === right) socket.emit("right", false);
+      if(event.key === run) socket.emit("running", false);
+   }
 });
 
 
@@ -39,12 +48,43 @@ window.addEventListener("keyup", (event) => {
 // =====================================================================
 socket.on("newPosition", (data) => {
    ctx.clearRect(0, 0, canvas.width, canvas.height);
-   ctx.fillStyle = "red";
    for(let i = 0; i < data.length; i++) {
-      ctx.fillRect(data[i].x, data[i].y, 80, 80);
+      let player = data[i];
+      
+      ctx.fillStyle = "red";
+      ctx.fillRect(player.x, player.y, player.width, player.height);
+      
+      ctx.fillStyle = "black";
+      ctx.font = "20px Verdana";
+      ctx.fillText(Math.floor(player.health), player.x + 20, player.y + 30);
    }
 });
 
+
+// =====================================================================
+// Chatting
+// =====================================================================
+const chatBox = document.querySelector(".chat-box"); 
+const chatForm = document.querySelector(".chat-form");
+const chatInput = document.querySelector(".chat-form input");
+
+chatInput.addEventListener("click", () => isChatting = true);
+canvas.addEventListener("click", () => isChatting = false);
+
+socket.on("addMessage", (data) => {
+   chatBox.innerHTML += `<p>${data}</p>`;
+});
+
+socket.on("evalResponse", (data) => {
+   console.log(data);
+});
+
+chatForm.onsubmit = (event) => {
+   event.preventDefault();
+   if(chatInput.value[0] === "/") socket.emit("evalServer", chatInput.value.slice(1));
+   else socket.emit("sendMessage", chatInput.value);
+   chatInput.value = "";
+}
 
 
 // =====================================================================
